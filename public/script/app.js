cb0bb5c7f204e5e055aff0398e445d0529571f5c
jezza <jeremygray001@gmail.com>
2017-03-05 18:48:39 -0700

    Created directives, initialized app, created index.html file.

--- public/script/app.js
+++ public/script/app.js
@@ -1,1 +1,23 @@
-angular.module("appName", []);
+angular.module("appName", [])
+.directive('headerMenu', function(){
+	return {
+		restrict: 'AE',
+		templateUrl: '/views/directives/header.html'
+
+	}
+}).directive('sideMenu', function() {
+	return {
+		restrict: 'AE',
+		templateUrl: '/views/home.html'
+	}
+}).directive('storyHolder', function() {
+	return {
+		restrict: 'AE',
+		templateUrl: '/views/directives/story-holder.html'
+	}
+}).directive('suggestWriters', function() {
+	return {
+		restrict: 'AE',
+		templateUrl: '/views/directives/suggest-writers.html'
+	}
+})
