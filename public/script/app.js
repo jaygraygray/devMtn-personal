bf906f3fa2ff4bf0452cf13ec12c02eee304f8ee
jezza <jeremygray001@gmail.com>
2017-03-06 11:43:52 -0700

    Added show/hide functionality for user and notifications menus

--- public/script/app.js
+++ public/script/app.js
@@ -2,9 +2,32 @@ angular.module("appName", [])
 .directive('headerMenu', function(){
 	return {
 		restrict: 'AE',
-		templateUrl: '/views/directives/header.html'
-
+		templateUrl: '/views/directives/header.html',
 	}
+}).directive('makeTagsSticky', function($window) {
+	var win = angular.element($window)
+	return {
+		restrict: 'AE',
+		link: function(scope, ele, attrs) {
+			var topClass = attrs.makeTagsSticky
+			var offsetTop = ele.prop('offsetTop') + 70;
+			console.log($window.pageYOffset)
+			win.on('scroll', function(e) {
+				ele[($window.pageYOffset >= offsetTop) ? 'addClass' : 'removeClass']('no-moving-for-you');
+			});
+		}
+	}
+//})
+// .directive('profileMenu', function() {
+// 	return {
+// 		restrict: 'AE',
+// 		template: '<img src="/images/profile.png" class="profile-picture">',
+// 		link: function(scope, ele, attr) {
+// 			ele.on("click", function() {
+				
+// 			})
+// 		}
+// 	}
 }).directive('sideMenu', function() {
 	return {
 		restrict: 'AE',
@@ -20,17 +43,4 @@ angular.module("appName", [])
 		restrict: 'AE',
 		templateUrl: '/views/directives/suggest-writers.html'
 	}
-}).directive('makeTagsSticky', function($window) {
-	var win = angular.element($window)
-	return {
-		restrict: 'AE',
-		link: function(scope, ele, attrs) {
-			var topClass = attrs.makeTagsSticky
-			var offsetTop = ele.prop('offsetTop') + 70;
-			console.log($window.pageYOffset)
-			win.on('scroll', function(e) {
-				ele[($window.pageYOffset >= offsetTop) ? 'addClass' : 'removeClass']('no-moving-for-you');
-			});
-		}
-	}
-})
+});
