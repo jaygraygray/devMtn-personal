d3bdbb8896e889d52c7be5e279e68459c8badb07
Jeremy Gray <jeremygray001@gmail.com>
2017-03-26 15:33:39 -0600

    Your Stories displays unpublished drafts correctly.

--- db/draft_get_all_ids.sql
+++ db/draft_get_all_ids.sql
@@ -1,3 +1,3 @@
 select distinct article_id from drafts 
-where author_id = $1
+where author_id = $1 AND published = false
 order by article_id
--- public/script/services/yourStoriesSvc.js
+++ public/script/services/yourStoriesSvc.js
@@ -4,11 +4,9 @@ function($http) {
 this.getAllDrafts = function(author_id) {
 	return $http.get('/api/getdrafts/' + author_id)
 	.then(function(resp) {
-		
 		return resp.data
 	})
 }
-
 
 
 })
